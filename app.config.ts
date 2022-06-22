744e18c02c5c908a9212ee20989b2f1226a38d71
Ivo <ivoprovensi1@gmail.com>
2022-06-22 14:03:39 -0400

    chore: move static config to app.json

--- app.config.ts
+++ app.config.ts
@@ -4,43 +4,17 @@ const IS_BETA = process.env.APP_VARIANT === "beta";
 
 export default ({ config }: ConfigContext): ExpoConfig => ({
   ...config,
-  assetBundlePatterns: ["**/*"],
-  scheme: "tamagui-firebase-mobx-template",
-  name: IS_DEV ? "[DEV] TFMT" : IS_BETA ? "Tamagui Beta" : "Tamagui",
-  slug: "tamaguifmt",
+  name: IS_DEV ? `[DEV] ${config.name}` : config.name,
+  slug: config.slug,
   version: "1.1.1",
-  orientation: "portrait",
   icon: IS_DEV
     ? "./assets/icons/icon-dev.png"
     : IS_BETA
     ? "./assets/icons/icon-beta.png"
     : "./assets/icons/icon.png",
-  userInterfaceStyle: "light",
-  splash: {
-    backgroundColor: "#ddffe2",
-    image: "./assets/splash.png",
-    resizeMode: "contain",
-  },
-  updates: {
-    fallbackToCacheTimeout: 0,
-  },
   android: {
     package: IS_DEV ? "dev.tamagui.expo.devclient" : "dev.tamagui.expo.client",
     googleServicesFile: "google-services.json",
     versionCode: 5,
   },
-  androidNavigationBar: {
-    backgroundColor: "#ddffe2",
-    barStyle: "dark-content",
-  },
-  androidStatusBar: {
-    backgroundColor: "#ddffe2",
-    barStyle: "dark-content",
-  },
-  plugins: [
-    "@react-native-firebase/app",
-    "@react-native-firebase/crashlytics",
-    "@react-native-firebase/perf",
-    "@react-native-google-signin/google-signin",
-  ],
 });
