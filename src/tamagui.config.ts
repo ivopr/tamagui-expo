4cc3252c40df4db9455ee8631f22da644c8a698f
Garrett Boatman <garrett@getrecord.com>
2023-01-12 23:48:06 -0800

    fix: provide matchMedia implementation (#17)

--- src/tamagui.config.ts
+++ src/tamagui.config.ts
@@ -2,6 +2,7 @@ import { createInterFont } from "@tamagui/font-inter";
 import { shorthands } from "@tamagui/shorthands";
 import { themes, tokens } from "@tamagui/theme-base";
 import { createTamagui } from "tamagui";
+import { createMedia } from '@tamagui/react-native-media-driver'
 
 import { animations } from "./constants/animations";
 
@@ -61,7 +62,7 @@ const config = createTamagui({
   },
   themes,
   tokens,
-  media: {
+  media: createMedia({
     xs: { maxWidth: 660 },
     sm: { maxWidth: 800 },
     md: { maxWidth: 1020 },
@@ -76,7 +77,7 @@ const config = createTamagui({
     tall: { minHeight: 820 },
     hoverNone: { hover: "none" },
     pointerCoarse: { pointer: "coarse" },
-  },
+  }),
 });
 
 export type AppConfig = typeof config;
