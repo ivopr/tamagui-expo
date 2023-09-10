68bea508787f8c4f077d8b89b01d6530aad5e710
Ivo <ivoprovensi1@gmail.com>
2023-09-10 09:52:18 -0400

    fix: added tamagui tabs example

--- app/tabs/tab2.tsx
+++ app/tabs/tab2.tsx
@@ -1,11 +1,38 @@
-import { Text } from "tamagui";
+import { H5, Tabs, Text } from "tamagui";
 
 import { MyStack } from "../../components/MyStack";
 
 export default function Tab2() {
   return (
     <MyStack>
-      <Text>This is Tab 2</Text>
+      <Tabs
+        defaultValue="tab1"
+        orientation="horizontal"
+        flexDirection="column"
+        width="100%"
+      >
+        <Tabs.List>
+          <Tabs.Tab value="tab1">
+            <Text>SubTab 1</Text>
+          </Tabs.Tab>
+          <Tabs.Tab value="tab2">
+            <Text>SubTab 2</Text>
+          </Tabs.Tab>
+          <Tabs.Tab value="tab3">
+            <Text>SubTab 3</Text>
+          </Tabs.Tab>
+        </Tabs.List>
+
+        <Tabs.Content value="tab1">
+          <H5>SubTab 1 Content</H5>
+        </Tabs.Content>
+        <Tabs.Content value="tab2">
+          <H5>SubTab 2 Content</H5>
+        </Tabs.Content>
+        <Tabs.Content value="tab3">
+          <H5>SubTab 3 Content</H5>
+        </Tabs.Content>
+      </Tabs>
     </MyStack>
   );
 }
